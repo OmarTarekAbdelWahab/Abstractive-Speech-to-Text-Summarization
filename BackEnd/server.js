ccba4087b460f49a266bc9ad33ea2efb065b46c2
Omar Tarek <omar.tarekabdelwahab@gmail.com>
2025-03-22 23:26:24 +0200

    Finished handling user registration and logging in.

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,22 +1,29 @@
 import dotenv from 'dotenv';
 import express, { json } from 'express';
+import cors from 'cors';
+import connectDB from './DataBase/DB.js'
+import userRoutes from './routes/uesr_api.js';
+
 import errorHandler from './middleware/errorHandler.js';
-import apiRoutes from './routes/api.js';
 
 dotenv.config();
 
+connectDB();
+
+
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(cors());
 app.use(json());
 
+// app.use(tokenVerifier);
 
-app.use('/api', apiRoutes);
-
-app.get('/', (req, res) => {
-  res.json({ message: 'Welcome to the backend!' });
-});
+app.use('/user', userRoutes);
 
 app.use(errorHandler);
+
+
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
